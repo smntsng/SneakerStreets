1cd00f8a74463dbff5d8c25691554180527ffa83
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-18 18:22:44 +0000

    Jsx files added with starter code

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,30 @@
+const Nav = () => {
+    return(
+        <>
+            <nav>
+                <div className="brandName">
+                <h1>SneakerStreets</h1>
+                </div>
+                <ul>
+                <li>
+                    <a href="_blank">Home</a>
+                </li>
+                <li>
+                    <a href="_blank">Picture</a>
+                </li>
+                <li>
+                    <a href="_blank">Video</a>
+                </li>
+                <li>
+                    <a href="_blank">About</a>
+                </li>
+                <li>
+                    <a href="_blank">Contact</a>
+                </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Nav;
---
+++ src/pages/Trending.jsx
@@ -0,0 +1,9 @@
+const Trending = () => {
+    return(
+        <>
+            <h2>Trending</h2>
+        </>
+    )
+}
+
+export default Trending;
