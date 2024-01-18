939580d846236ac85459d09652f612a7ea73f476
Josh <j.prestage@hotmail.com>
2024-01-18 19:22:14 +0000

    Product page folder update

---
+++ src/components/Product Page/Product-page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import product-page from './product-page.module.scss'
+
+const Product-page = () => {
+  return (
+    <div>Product-page</div>
+  )
+}
+
+export default Product-page
