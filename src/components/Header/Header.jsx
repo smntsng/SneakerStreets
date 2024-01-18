c97a01e088ca734df64c3caa7d2d7ffca608bb87
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-18 19:11:54 +0000

    examples

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import Nav from "../Nav/Nav";
+import './header.css';
+
+import isIcon from '../../assets/images/icon.png'
 
 const Header = () => {
     return(
         <>
             <h2>Header</h2>
+            <img src={isIcon} ></img>
             <Nav />
         </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import '../assets/css/style.css';
 const Home = () => {
     return(
         <>
