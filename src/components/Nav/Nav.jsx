9468718c524f47acad9118f4730c0ac687f44b4d
clemens <dcleme51@gmail.com>
2024-01-21 22:50:12 +0000

    updating nav bare and packagejson

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,34 +1,47 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/images/Logo1.png'
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import './nav.css'
 const Nav = () => {
-    return(
-        <>
-            <nav>
+    return (
+        <header className="header">
+            <div className="Header-top-row">
+                <p>Info</p>
+                <p>Login</p>
+            </div>
+            <div className="header-bottom-row">
                 <div className="logo">
-                <img src={logo} style={{height:'100px'}}></img>
+                    <img src={logo} style={{ height: '200px' }}></img>
                 </div>
-                <ul>
-                <li>
-                    <NavLink to="/" end className="nav-link" > Home </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/trending" end className="nav-link" >Trending</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/products" end className="nav-link" > Products </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/productpage" end className="nav-link" > Products Page </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login" end className="nav-link" > Login </NavLink>
-                </li>
-                <li>
-                    <a href="_blank">Contact</a>
-                </li>
-                </ul>
-            </nav>
-        </>
+                <nav className="navbar">
+
+                    <ul className="nav-links">
+                        <li>
+                            <NavLink to="/" end className="nav-link" > Home </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/trending" end className="nav-link" >Trending</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/products" end className="nav-link" > Products </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login" end className="nav-link" > Login </NavLink>
+                        </li>
+                        <li>
+                            <a href="_blank">Contact</a>
+                        </li>
+                    </ul>
+
+                </nav>
+                <div className="header-nav-icons">
+                     <FaSearch /> 
+                     <FaShoppingCart />
+                </div>
+            </div>
+
+        </header>
     )
 }
 export default Nav;
