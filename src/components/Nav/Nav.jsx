97fd34eb470c275a0d6ecabbd2bf0d19dd48da66
clemens <dcleme51@gmail.com>
2024-01-21 16:37:20 +0000

    nav-bar and styling pt1

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,31 +1,45 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/images/Logo1.png'
+import './nav.css'
 const Nav = () => {
-    return(
-        <>
-            <nav>
+    return (
+        <header className="header">
+            <div className="Header-top-row">
+                <p>Info</p>
+                <p>Login</p>
+            </div>
+            <div className="header-bottom-row">
                 <div className="logo">
-                <img src={logo} style={{height:'100px'}}></img>
+                    <img src={logo} style={{ height: '200px' }}></img>
                 </div>
-                <ul>
-                <li>
-                    <NavLink to="/" end className="nav-link" > Home </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/trending" end className="nav-link" >Trending</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/products" end className="nav-link" > Products </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login" end className="nav-link" > Login </NavLink>
-                </li>
-                <li>
-                    <a href="_blank">Contact</a>
-                </li>
-                </ul>
-            </nav>
-        </>
+                <nav className="navbar">
+
+                    <ul className="nav-links">
+                        <li>
+                            <NavLink to="/" end className="nav-link" > Home </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/trending" end className="nav-link" >Trending</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/products" end className="nav-link" > Products </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/login" end className="nav-link" > Login </NavLink>
+                        </li>
+                        <li>
+                            <a href="_blank">Contact</a>
+                        </li>
+                    </ul>
+
+                </nav>
+                <div className="header-nav-icons">
+                    <p>Search</p>
+                    <p>Basket</p>
+                </div>
+            </div>
+
+        </header>
     )
 }
 export default Nav;
