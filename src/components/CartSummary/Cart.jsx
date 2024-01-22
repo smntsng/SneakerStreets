bf8f3b595c854a870b465ac6ccbe619f04dc08a3
smntsng <144932270+smntsng@users.noreply.github.com>
2024-01-22 16:01:47 +0000

    updated checkout page with checkout button and cart container

--- src/components/CartSummary/Cart.jsx
+++ src/components/CartSummary/Cart.jsx
@@ -5,13 +5,32 @@ import './Cart.css';
 const Cart = () => {
     return(
         <>
-        <h1>cart component</h1>
+        <h1>Checkout</h1>
 
-        <button>Pay Now</button>
+        <div className='summary'>
+        <h2>
+            Total:
+            {/* need to add price logic here */}
+        </h2>
+
+        <button className="checkoutButton"> CHECKOUT</button>
+        </div>
+    <div className='atcContainer'>
+        <div className='atcproducts'>
+        <h2>
+            Cart
+        </h2>
+
+        <div className='products'>
+            products that have been added to Cart
+            {/* atc logic */}
+        </div>
+        </div>
+        </div>
         </>
     )
 }
 
 
 
-export default Cart
+export default Cart 
