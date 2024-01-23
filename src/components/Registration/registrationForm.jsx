bf84f5662f095497e194026d3c8294fa5b26382d
Ali Halavurt <alihalavurt@Alis-MacBook-Pro.local>
2024-01-23 15:15:10 -0300

    added registration form jsx and css

---
+++ src/components/Registration/registrationForm.jsx
@@ -0,0 +1,28 @@
+import Container from "react-bootstrap/Container";
+import './registrationForm.css'
+const RegisterForm = () => {
+    return(
+        <>
+            <Container fluid className="registerContainer">
+                    <p style = {{   fontFamily: 'Monument Extended,sans-serif',
+                                textTransform: 'uppercase',
+                                fontSize: '1.4rem',
+                                letterSpacing: '.1rem'}}>Register
+                    </p>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputEmail1">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-primary" id="loginBtn">Login</button>
+                    </form>
+                </Container>
+        </>
+    )
+}
+export default RegisterForm
