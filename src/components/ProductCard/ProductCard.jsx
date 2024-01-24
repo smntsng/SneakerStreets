e3769fffae7f44c1693099ff411cf99148fe38c0
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-24 15:07:12 +0000

    product card linked to products page

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -12,51 +12,53 @@ const ProductCard = props => {
     return(
         <>
                 <div className="" style={{display:'contents'}}>
-                    <div className="card">
-                        <div className='specialTag'>
-                            {specialTag && 
-                                <span><img src={hotSale} /></span>
-                            }
-                        </div>
-                        <img className="card-img" src={image} alt="Vans" />
-                        <div className="d-flex justify-content-end">
-                            <a href="#" className="card-link text-danger like">
-                                <i className="fas fa-heart"></i>
-                                {/* Heart Icon  */}
-                            </a>
-                        </div>
-                        <div className="card-body">
-                            <h4 className="card-title">{title}</h4>
-                            <h6 className="card-brand mb-2 text-muted">{brand}</h6>
-                            <h6 className="card-subtitle mb-2 text-muted">Style: {styleCode}</h6>
-                            <div className="options d-flex flex-fill">
-                                <div className="price text-success">
-                                    <h5 className="mt-4">
-                                        £{price}
-                                    </h5>
-                                </div>
+                        <div className="card">
+                            <div className='specialTag'>
+                                {specialTag && 
+                                    <span><img src={hotSale} /></span>
+                                }
                             </div>
-                            <div className="buy d-flex justify-content-between align-items-baseline">
-                                <select className="custom-select mr-1">
-                                    <option defaultValue>Color</option>
-                                    {colourOption.map( (color, index) => {
-                                        <option key={index} value={index}>{color}</option>
-                                    })}
-                                    
-                                    
-                                </select>
-                                <select className="custom-select ml-1">
-                                    <option defaultValue>Size</option>
-                                    <option value="1">41</option>
-                                    <option value="2">42</option>
-                                    <option value="3">43</option>
-                                </select>
-                                <a href="#" className="btn cartBtn mt-3">Add
-                                    <FontAwesomeIcon icon="fa-regular fa-mug-hot" />
+                            <img className="card-img" src={image} alt="Vans" />
+                            <div className="d-flex justify-content-end">
+                                <a href="#" className="card-link text-danger like">
+                                    <i className="fas fa-heart"></i>
+                                    {/* Heart Icon  */}
                                 </a>
                             </div>
+                            <div className="card-body">
+                            <Link to="/productpage" end className="nav-link" >  
+                                <h4 className="card-title">{title}</h4>
+                            </Link>
+                                <h6 className="card-brand mb-2 text-muted">{brand}</h6>
+                                <h6 className="card-subtitle mb-2 text-muted">Style: {styleCode}</h6>
+                                <div className="options d-flex flex-fill">
+                                    <div className="price text-success">
+                                        <h5 className="mt-4">
+                                            £{price}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div className="buy d-flex justify-content-between align-items-baseline">
+                                    <select className="custom-select mr-1">
+                                        <option defaultValue>Color</option>
+                                        {colourOption.map( (color, index) => {
+                                            <option key={index} value={index}>{color}</option>
+                                        })}
+                                        
+                                        
+                                    </select>
+                                    <select className="custom-select ml-1">
+                                        <option defaultValue>Size</option>
+                                        <option value="1">41</option>
+                                        <option value="2">42</option>
+                                        <option value="3">43</option>
+                                    </select>
+                                    <a href="#" className="btn cartBtn mt-3">Add
+                                        <FontAwesomeIcon icon="fa-regular fa-mug-hot" />
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
                 </div>
         </>
     )
