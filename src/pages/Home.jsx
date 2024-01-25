d3c7a572de9d570b295d4c7910aede9443fec2d8
clemens <dcleme51@gmail.com>
2024-01-25 03:06:35 +0000

    working on modal and using promoimage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import sneakers from '../assets/json/sneakers'
 import ProductCard from '../components/ProductCard/ProductCard';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { FaTimes } from 'react-icons/fa';
 import { useState, useEffect } from "react";
 const Home = () => {
     const [modalShow, setModalShow] = useState(false);
@@ -12,31 +13,28 @@ const Home = () => {
         return (
             <Modal
                 {...props}
-                size="lg"
+                size="md"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
             >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Modal heading
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="discountModal">
-                    <h4>Exclusive Offer</h4>
-                    <p>
-                        Welcome to SneakerStreet.
-                    </p>
+                <div className="modal-header">
+                    <div className="close-button">
+                        <Button onClick={props.onHide} className="close-button">
+                            <FaTimes size={10} />
+                        </Button>
+                    </div>
+                </div>
+                <Modal.Body>
+                    <img src="src\assets\images\PromoModal.png"></img>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
             </Modal>
         );
+
     }
     useEffect(() => {
         const fetchModal = async () => {
-          await new Promise((resolve) => setTimeout(resolve, 3500));
-          setModalShow(true);
+            await new Promise((resolve) => setTimeout(resolve, 3500));
+            setModalShow(true);
         };
 
 
@@ -47,8 +45,8 @@ const Home = () => {
         <>
             <div style={{ width: '80%', margin: '0 auto', maxWidth: '1400px' }}>
                 <Hero />
-                <MyVerticallyCenteredModal 
-                    show={modalShow} 
+                <MyVerticallyCenteredModal
+                    show={modalShow}
                     onHide={() => setModalShow(false)}
                 />
                 <h4 className='containerHeading'>New Arrivals</h4>
