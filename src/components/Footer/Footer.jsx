5de5b9ab34ab8f996caf960ed13895488dcfa465
Josh <j.prestage@hotmail.com>
2024-01-25 16:11:05 +0000

    Styling update 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,17 +17,17 @@ const Footer = () => {
                 <div className= "footer-section-menu text-start">
                     <h3><strong>Menu</strong></h3>
                     <ul>
-                        <li><a href="" >About Us</a></li>
-                        <li><a href="">Products</a> </li>
-                        <li><a href="">Orders</a></li>
-                        <li><a href="">Shipping Info</a></li>
-                        <li><a href="">FAQs</a></li>
+                        <li><a href="#" >About Us</a></li>
+                        <li><a href="#">Products</a> </li>
+                        <li><a href="#">Orders</a></li>
+                        <li><a href="#">Shipping Info</a></li>
+                        <li><a href="#">FAQs</a></li>
                     </ul>
                 </div>
                 <div className= "footer-section-contact text-start ">
                     <h3><strong>Get in Touch</strong></h3>
                     <ul>
-                        <li><FontAwesomeIcon icon={faHouse} /> {""} New York, NY 2024, US</li>
+                        <li ><a href="https://www.google.com/maps/place/2024+US-1,+Rahway,+NJ+07065,+USA/@40.6075688,-74.2640265,17z/data=!3m1!4b1!4m6!3m5!1s0x89c3b3770cf143eb:0xdbad815d75b3bda1!8m2!3d40.6075648!4d-74.2614516!16s%2Fg%2F11bw4hmsy2?entry=ttu" target="blank"><FontAwesomeIcon icon={faHouse} /> {""} New York, NY 2024, US</a></li>
                         <li><a href="mailto: hello@sneakerstreets.com"><FontAwesomeIcon icon={faEnvelope} /> {""} hello@sneakerstreets.com</a></li>
                         <li><a href="tel:+ 01 453 069 00"><FontAwesomeIcon icon={faPhone} /> {""}+ 01 453 069 00</a> </li>
                     </ul>
@@ -43,10 +43,10 @@ const Footer = () => {
                         </div>
                     </section>
                 <div className= "socials">
-                    <div className='socialIcon'> <FontAwesomeIcon icon={faFacebook} /></div>
-                    <div className='socialIcon'> <FontAwesomeIcon icon={faTwitter}  /></div>
-                    <div className='socialIcon'> <FontAwesomeIcon icon={faTiktok} /></div>
-                    <div className='socialIcon'> <FontAwesomeIcon icon={faInstagram} /></div>
+                    <a href="https://www.facebook.com/" target='blank'><div className='socialIcon'> <FontAwesomeIcon icon={faFacebook} /></div></a>
+                    <a href="https://twitter.com/" target='blank'><div className='socialIcon'> <FontAwesomeIcon icon={faTwitter}  /></div></a> 
+                    <a href="https://www.tiktok.com/" target='blank'><div className='socialIcon'> <FontAwesomeIcon icon={faTiktok} /></div></a>
+                    <a href="https://www.instagram.com/" target='blank'><div className='socialIcon'> <FontAwesomeIcon icon={faInstagram} /></div></a>
                 </div>
             </div>
             </div>
