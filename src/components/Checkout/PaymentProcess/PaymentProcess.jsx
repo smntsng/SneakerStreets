07ade187fff42b138ea56ecbaf095325cfc98be8
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-26 17:24:01 +0000

    added secure checkout page

--- src/components/Checkout/PaymentProcess/PaymentProcess.jsx
+++ src/components/Checkout/PaymentProcess/PaymentProcess.jsx
@@ -1,4 +1,4 @@
-import { Button } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
 
 const PaymentProcess = props => {
     return(
@@ -10,11 +10,9 @@ const PaymentProcess = props => {
                 <p><strong>Total: </strong> <span>£{(props.total).toFixed(2)}</span></p>
                 <hr/>
             </div>
-            <div className="checkoutOptions">
-                <button
-                    className="upDown bg-violet-600 py-3 px-5 rounded-lg text-white text-1xl"
-                    > Checkout securely
-                </button>
+            <div className="checkoutOptions" style={{margin: '10px 0px'}}>
+                <NavLink to="/securecheckout" end className="bg-violet-600 py-3 px-5 rounded-lg text-white text-1xl" > Secure Checkout </NavLink>
+
                 <p>We accept the following payment methods</p>
                 <img className="paymenyIcon" src="https://www.jdsports.co.uk/skins/jdsportsuk-desktop/public/img/cards/cards-gb.png"></img>
                 <img className="paymenyIn" src="https://www.jdsports.co.uk/skins/default/public/img/cards/paypal-one.png"></img>
