e5e4eb95df66be94d1bf9ad92b6923deb5932d75
Ali Halavurt <alihalavurt@Alis-MacBook-Pro.local>
2024-01-26 13:38:58 -0300

    removed the bootstrap card testing and reverted to original cards

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,29 +1,8 @@
 import './categoryCard.css'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 const CategoryCard = (props) => {
   return(
       <>
-      <Card className="bg-dark text-white">
-      <Card.Img src= {props.img} className= "card-image" />
-      <Card.ImgOverlay>
-        
-        <Card.Text>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
-        </Card.Text>
-        <Card.Text>Last updated 3 mins ago</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.ImgOverlay>
-    </Card>
-   
-
-
-
-
-
-
-      {/* <div className= "card-container">
+      <div className= "card-container">
         <div className= "card-body">
           <img src= {props.img} className= "card-image"/>
           <p className= "cardDescription"> {props.description}</p>
@@ -31,7 +10,7 @@ const CategoryCard = (props) => {
          
         </div>
         
-      </div> */}
+      </div>
           
           
       </>
