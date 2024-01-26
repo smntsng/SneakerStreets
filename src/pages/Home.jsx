ca761028230a983fb9fb902f717eb5279f3270dd
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-26 02:21:28 +0000

    Promo modal moved into Modal.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,53 +3,27 @@ import Hero from '../components/Hero/Hero';
 //import dynamic data
 import sneakers from '../assets/json/sneakers'
 import ProductCard from '../components/ProductCard/ProductCard';
-import Modal from 'react-bootstrap/Modal';
-import { FaTimes } from 'react-icons/fa';
-import { useState, useEffect } from "react";
 import CategoryCard from '../components/CategoryCard/CategoryCard';
-import { Button } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import ModalDiscount from '../components/ModalDiscount/ModalDiscount';
 
 
 const Home = () => {
+    //Discount Modal
     const [modalShow, setModalShow] = useState(false);
-    function MyVerticallyCenteredModal(props) {
-        return (
-            <Modal
-                {...props}
-                size="md"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Body>
-                    <div className="modal-header" style={{position: 'absolute', right: '0px', backgroundColor:'transparent'}}>
-                        <div className="close-button">
-                            <Button onClick={props.onHide} className="close-button" style={{padding: '10px'}}>
-                                <FaTimes size={12} />
-                            </Button>
-                        </div>
-                    </div>
-                    <img src="src\assets\images\PromoModal.png"></img>
-                </Modal.Body>
-            </Modal>
-        );
-
-    }
     useEffect(() => {
         const fetchModal = async () => {
             await new Promise((resolve) => setTimeout(resolve, 3500));
-            setModalShow(true);
+            setModalShow(true)
         };
-
-
         fetchModal();
     }, []);
 
-    
     return (
         <>
             <div style={{ width: '90%', margin: '0 auto', maxWidth: '1300px' }}>
                 <Hero />
-                <MyVerticallyCenteredModal
+                <ModalDiscount
                     show={modalShow}
                     onHide={() => setModalShow(false)}
                 />
@@ -63,7 +37,6 @@ const Home = () => {
                     <h3 style={{marginTop: '30px'}} className= "categories-header">CATEGORIES</h3>
                 </div>
                 <div className= "wrapper">
-                  
                     <CategoryCard category="Men" img= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRsTvA_0zzi0UNvSxAE9CSObi90WXY4cIzDQ&usqp=CAU" description= "Find the latest trends for Men here!"/>
                     <CategoryCard category="Women" img= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjiAQL7vR2e7dx6OGjqogArFyPwyUJWezcXg&usqp=CAU" description= "Find the latest trends for Women here!"/>
                     <CategoryCard category="Kids" img= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUxk40lziGTex3LPmjxiuiaqriUSVwffrqOA&usqp=CAU" description= "Find the latest trends for Kids here!"/>
